861ba0b39bef3651bd1a16aee9e530e3ced236b6
My Name <Me@gmail.com>
2022-10-15 11:55:40 +0400

    3 commit

--- programming_task_2.py
+++ programming_task_2.py
@@ -1,14 +1,14 @@
-n = 36
+n = 37
 list1 = [i for i in range(1, n+1)]
 list2 = []
 list3 = []
 for i in list1:
     if n % i == 0:
         list2.append(i)
+    else:
+        del list1 [1:-1]
 
-list2 = [i for i in list2[1:-1:]]
-
-
+list2 = [i for i in list2[1:]]
 
 print(list2)
 print(list1)
